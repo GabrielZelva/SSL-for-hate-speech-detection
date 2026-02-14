f706b5607b17c99f887bd7c677c2284cba7ba76e
GabrielZelva <66466481+GabrielZelva@users.noreply.github.com>
2026-02-14 21:47:12 +0100

    Hand-in

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
-I am trying to learn about self supervised learning.
-I am doing it on hate-speech detection. 
+# Evaluating the use of Self-Supervised learning for Hate Speech detection
 
-This is the data:
-https://www.kaggle.com/datasets/mrmorj/hate-speech-and-offensive-language-dataset
+**Author**: Gabriel Pišvejc
+**Contact**: GabrielPisvejc@Protonmail.com, [LinkedIn](https://www.linkedin.com/in/gabrielpisvejc/?profileId=ACoAADo5FU0BnBPkBj5SmMESVBJ633BazdnsESU)
+
+This little project sort of accidentally happened when I was doing what was supposed to be a *15 minutes homework* for the [Machine Learning 2 course on my Master's](https://www.ehu.eus/en/web/master/master-language-analysis-processing/subject?redirect=consultaAsignatura&anyo-oferta-docente=20250&anyo-propuesta=20210&centro=226&materia=10447&asignatura=505303&tipo-asignatura=1&master=780). However, then I got a bit too interested in the algorithm and thought... I could implement this myself...
+
+The main experiment is documented in the [Jupyter notebook](https://github.com/GabrielZelva/SSL-for-hate-speech-detection/blob/main/experiment.ipynb), however, I am importing a few custom functions from the auxiliary scripts.
+
+The data used for the experiment is available [here](https://www.kaggle.com/datasets/mrmorj/hate-speech-and-offensive-language-dataset). 
+
--- experiment.ju.py
+++ experiment.ju.py
@@ -13,9 +13,9 @@
 # %% [md]
 # First things first, we will need to load the [data](https://www.kaggle.com/datasets/mrmorj/hate-speech-and-offensive-language-dataset). While the original dataset comes with some additional information about the labeling process and the text appears in raw form, I have already pre-processed it outside of this notebook.
 
-# Particularly, I only maintained the labels and the text, as these are the two variables of interest for this report and I already passed the raw text through the transformer blocks of the [all-MiniLM-L6-v2 model](https://huggingface.co/sentence-transformers/all-MiniLM-L6-v2) which was designed to return the sentence embeddings. While it would be interesting to fine-tune this model for the task at hand, I decided against it as I am working on a small cuda-less notebook. Therefore, I only used the model to get the embeddings for each datapoint and we will do the SSL experiments using a custom head for the model.
+# Particularly, I only maintained the labels and the text, as these are the two variables of interest for this report and I already passed the raw text through the transformer blocks of the [all-MiniLM-L6-v2 model](https://huggingface.co/sentence-transformers/all-MiniLM-L6-v2) which was designed to return the sentence embeddings. While it would be interesting to fine-tune this model for the task at hand, I decided against it as I am working on a small cuda-less notebook. Therefore, I only used the model to get the embeddings for each datapoint and I will do the SSL experiments using a custom head for the model.
 
-# For more information on the data preprocessing, see the `process_data.py` script.
+# For more information on the pre-preprocessing, see the `process_data.py` script.
 
 # %%
 import pandas as pd
@@ -36,7 +36,7 @@ data.iloc[:, len(data.columns) - 1].value_counts(normalize=True)
 # 2 - Neither
 
 # %% [md]
-# In order to prevent these issues, I have created 2 custom functions to extract and mask certain proportions of the data label-wise. That is, if I decide to do 50% masking, it will mask 50% of each label, rather then doing it blindly. The second function works in a similar fashion, however, is used to create splits. Therefore if I want to create a 5% split, it will pull 5% of the datapoints from each category. 
+# In order to prevent these issues, I have created 2 custom functions to extract and mask certain proportions of the data label-wise. That is, if I decide to do 50% masking, it will mask 50% of each label, rather then doing it blindly. The second function works in a similar fashion, however, it is used to create splits. Therefore if I want to create a 5% split, it will pull 5% of the datapoints from each category. 
 
 # Having said that, we will create the test split on 10% of the full dataset before masking. The train and dev sizes will be defined dynamically, as we will try the algorithm for different proportions of the unlabeled data, however, the dev split will always be 10% of the train split size.
 
@@ -55,100 +55,138 @@ test_Y = torch.tensor(test.values[:, -1], dtype=torch.long)
 # %% [md]
 # Aside from the data, I need a model to play with, or to be more precise, the head of the model, as we have already established that the transformer blocks and embeddings will stay intact.
 
-# Having said that, I defined the head class in the `model_head.py` script in order to keep this notebook clean. We can use it to get a baseline with no masked data whatsoever, that is, the ideal scenario.
+# Having said that, I defined the head class in the `model_head.py` script in order to keep this notebook clean. We can use it to get a baseline with no masked data whatsoever, that is, the ideal scenario. Additionally, I will be using Monte-Carlo estimation methods to get a better estimate for the evaluation metrics. 
 
-# %%
-from model_head import ModelHead
-from torch.utils.data import WeightedRandomSampler, TensorDataset, DataLoader
-
-experiment_data = data.copy()
-
-train, dev = extract_equal_proportion(experiment_data, proportion=0.1)
-dev_X = torch.tensor(dev.values[:, :-1], dtype=torch.float32)
-dev_Y = torch.tensor(dev.values[:, -1], dtype=torch.long)
-
-train_X = train.iloc[:, :-1]
-train_Y = train.iloc[:, -1]
-
-train_X = torch.tensor(train_X.values, dtype=torch.float32)
-train_Y = torch.tensor(train_Y.values, dtype=torch.long)
-
-train_dataset = TensorDataset(train_X, train_Y)
-train_loader = DataLoader(train_dataset, batch_size=32)
-
-model = ModelHead()
-
-model.train(train_loader, dev_X, dev_Y)
-
-# %% [md]
 # I have also written a custom evaluation function which returns the overall accuracy and per label recall. More information can once again be found in the the `experiment_helpers.py` script.
 
 # %%
+import numpy as np
+from model_head import ModelHead
+from torch.utils.data import WeightedRandomSampler, TensorDataset, DataLoader
 from experiment_helpers import evaluate_model
 
-predictions = model.predict(test_X, return_predictions=True)
 
-accuracy, recall_0, recall_1, recall_2 = evaluate_model(
-    model=model, predictions=predictions, data=test_X, ground_truth=test_Y
-)
+baseline_acc = np.array([], dtype=float)
+baseline_rec0 = np.array([], dtype=float)
+baseline_rec1 = np.array([], dtype=float)
+baseline_rec2 = np.array([], dtype=float)
 
-print(f"Overall accuracy: {accuracy:.3f}")
-print(f"Recall on hate speech: {recall_0:.3f}")
-print(f"Recall on offensive language: {recall_1:.3f}")
-print(f"Recall on neither: {recall_2:.3f}")
+for i in range(30):
+
+    experiment_data = data.copy()
+
+    experiment_data = experiment_data.sample(frac=1).reset_index(drop=True)
+
+    train, dev = extract_equal_proportion(experiment_data, proportion=0.1)
+    dev_X = torch.tensor(dev.values[:, :-1], dtype=torch.float32)
+    dev_Y = torch.tensor(dev.values[:, -1], dtype=torch.long)
+
+    train_X = train.iloc[:, :-1]
+    train_Y = train.iloc[:, -1]
+
+    train_X = torch.tensor(train_X.values, dtype=torch.float32)
+    train_Y = torch.tensor(train_Y.values, dtype=torch.long)
+
+    train_dataset = TensorDataset(train_X, train_Y)
+    train_loader = DataLoader(train_dataset, batch_size=32)
+
+    model = ModelHead()
+
+    model.train(train_loader, dev_X, dev_Y)
+
+    predictions = model.predict(test_X, return_predictions=True)
+
+    accuracy, recall_0, recall_1, recall_2 = evaluate_model(
+        model=model, predictions=predictions, data=test_X, ground_truth=test_Y
+    )
+
+    baseline_acc = np.append(baseline_acc, accuracy)
+    baseline_rec0 = np.append(baseline_rec0, recall_0)
+    baseline_rec1 = np.append(baseline_rec1, recall_1)
+    baseline_rec2 = np.append(baseline_rec2, recall_2)
+
+print(f"Accuracy estimation: {baseline_acc.mean():.3} +- {np.std(baseline_acc):.3}")
+print(f"Recall on Hate speech estimation: {baseline_rec0.mean():.3} +- {np.std(baseline_rec0):.3}")
+print(f"Recall on Offensive language estimation: {baseline_rec1.mean():.3} +- {np.std(baseline_rec1):.3}")
+print(f"Recall on Neither estimation: {baseline_rec2.mean():.3} +- {np.std(baseline_rec2):.3}")
 
 # %% [md]
-# We can notice that even before dealing with missing labels, the model finds it hard to deal with the data imbalance. We can therefore oversample the minority classes in order to reduce this problem. 
+
+# We can notice that even before dealing with missing labels, the model finds it hard to deal with the data imbalance. We can therefore oversample the minority classes in order to reduce this problem.
 
 # %%
-experiment_data = data.copy()
 
-train, dev = extract_equal_proportion(experiment_data, proportion=0.1)
+oversamp_acc = np.array([], dtype=float)
+oversamp_rec0 = np.array([], dtype=float)
+oversamp_rec1 = np.array([], dtype=float)
+oversamp_rec2 = np.array([], dtype=float)
 
-dev_X = torch.tensor(dev.values[:, :-1], dtype=torch.float32)
-dev_Y = torch.tensor(dev.values[:, -1], dtype=torch.long)
+for i in range(30):
 
-train_X = train.iloc[:, :-1]
-train_Y = train.iloc[:, -1]
+    experiment_data = data.copy()
 
-train_X = torch.tensor(train_X.values, dtype=torch.float32)
-train_Y = torch.tensor(train_Y.values, dtype=torch.long)
+    experiment_data = experiment_data.sample(frac=1).reset_index(drop=True)
 
-class_counts = torch.bincount(train_Y)
-class_weights = 1.0 / class_counts.float()
-sample_weights = class_weights[train_Y]
+    train, dev = extract_equal_proportion(experiment_data, proportion=0.1)
 
-sampler = WeightedRandomSampler(
-    weights=sample_weights, num_samples=len(sample_weights), replacement=True
-)
+    dev_X = torch.tensor(dev.values[:, :-1], dtype=torch.float32)
+    dev_Y = torch.tensor(dev.values[:, -1], dtype=torch.long)
 
-train_dataset = TensorDataset(train_X, train_Y)
-train_loader = DataLoader(train_dataset, batch_size=32, sampler=sampler)
+    train_X = train.iloc[:, :-1]
+    train_Y = train.iloc[:, -1]
 
-model = ModelHead()
+    train_X = torch.tensor(train_X.values, dtype=torch.float32)
+    train_Y = torch.tensor(train_Y.values, dtype=torch.long)
 
-model.train(train_loader, dev_X, dev_Y)
+    class_counts = torch.bincount(train_Y)
+    class_weights = 1.0 / class_counts.float()
+    sample_weights = class_weights[train_Y]
 
-predictions = model.predict(test_X, return_predictions=True)
+    sampler = WeightedRandomSampler(
+        weights=sample_weights, num_samples=len(sample_weights), replacement=True
+    )
 
-accuracy, recall_0, recall_1, recall_2 = evaluate_model(
-    model=model, predictions=predictions, data=test_X, ground_truth=test_Y
-)
+    train_dataset = TensorDataset(train_X, train_Y)
+    train_loader = DataLoader(train_dataset, batch_size=32, sampler=sampler)
 
-print(f"Overall accuracy: {accuracy:.3f}")
-print(f"Recall on hate speech: {recall_0:.3f}")
-print(f"Recall on offensive language: {recall_1:.3f}")
-print(f"Recall on neither: {recall_2:.3f}")
+    model = ModelHead()
+
+    model.train(train_loader, dev_X, dev_Y)
+
+    predictions = model.predict(test_X, return_predictions=True)
+
+    accuracy, recall_0, recall_1, recall_2 = evaluate_model(
+        model=model, predictions=predictions, data=test_X, ground_truth=test_Y
+    )
+
+    oversamp_acc = np.append(oversamp_acc, accuracy)
+    oversamp_rec0 = np.append(oversamp_rec0, recall_0)
+    oversamp_rec1 = np.append(oversamp_rec1, recall_1)
+    oversamp_rec2 = np.append(oversamp_rec2, recall_2)
+
+print(f"Accuracy estimation: {oversamp_acc.mean():.3} +- {np.std(oversamp_acc):.3}")
+print(f"Recall on Hate speech estimation: {oversamp_rec0.mean():.3} +- {np.std(oversamp_rec0):.3}")
+print(f"Recall on Offensive language estimation: {oversamp_rec1.mean():.3} +- {np.std(oversamp_rec1):.3}")
+print(f"Recall on Neither estimation: {oversamp_rec2.mean():.3} +- {np.std(oversamp_rec2):.3}")
 
 # %% [md]
 
-# # Do a test
+# While the means and standard deviations shown above do seem to show a level of improvement, the propper way to evaluate this is a statistical test. In this particular example, we will use the Wilcoxon rank-sum test:
+
+# %%
+from scipy import stats
+
+stat, p = stats.mannwhitneyu(baseline_rec0, oversamp_rec0, alternative="less")
+
+# I am so used to R's wilcox.test() I cannot bring myself to calling it Mann-whitney 
+print(f"Wilcoxon rank-sum test p-value: {p:.3}") 
+
 
 # %% [md]
 
-# Having established that, we can proceed to the actual SSL experiments.
+# As the test seems to indicate that the oversampling is indeed helping, I will keep using it in the SSL experiments. Speaking of those, the procedure will be as follows:
 
-# We will mask different percentages of the data and then train 2 models in each scenario. One only using the labeled data, and one leveraging the power of self-training algorithm. We will then analyse how the performance of the two compares across the different scenarios. 
+# I will mask different percentages of the data and then train 2 models in each scenario. One only using the labeled data, and one leveraging the power of the self-training algorithm. I will then analyse how the performance of the two compares across the different scenarios. 
 
 
 # %%
@@ -348,5 +386,5 @@ plt.show()
 
 # %% [md]
 
-# We can notice that on this particular dataset, the self-training algorithm doesn't seem to be very helpful. 
 
+# While SSL seems to have improved the recall on the minority class, it did so at a cost of overall accuracy and Offensive language recall. 
