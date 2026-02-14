b0d170226747372c9e9d5992566aa7fb68209f36
GabrielZelva <66466481+GabrielZelva@users.noreply.github.com>
2026-02-14 15:56:42 +0100

    The SSL experiments part should be done
    Now, I decided I will also incorporate the hypothesis testing homework in there.

--- experiment.ju.py
+++ experiment.ju.py
@@ -6,14 +6,14 @@
 
 # Using exTwitter data, I try to assess the possibility of using non labeled data to improve a transformer-encoder based model for classification.
 
-# In particular, we will try to predict whether posts are considered hate speech, offensive language or neither. This has a level of difficulty, as the the two categories of interest of interest often overlap. The distinction is however very important as offensive language is but a cultural perception of certain words as less prestigious, while hate speech can go as far as to be a criminal offense in certain jurisdictions and is usually used in order to discriminate. There is an important difference between saying *let's fucking do this* and *the fucking [ethnicity of your choice] did this*.
+# In particular, we will try to predict whether posts are considered hate speech, offensive language or neither. This has a level of difficulty, as the the two categories of interest often overlap. The distinction is however very important as offensive language is but a cultural perception of certain words as less prestigious, while hate speech can go as far as to be a criminal offense in certain jurisdictions and is usually used in order to discriminate. There really is an important difference between saying *let's fucking do this* and *the fucking [ethnicity of your choice] did this*.
 
 # --------------
 
 # %% [md]
-# First things first, we will need to load the [data](https://www.kaggle.com/datasets/mrmorj/hate-speech-and-offensive-language-dataset). While the original dataset comes with some additional information about the labeling process and the text appears in raw form, I have already preprocessed it outside of this notebook.
+# First things first, we will need to load the [data](https://www.kaggle.com/datasets/mrmorj/hate-speech-and-offensive-language-dataset). While the original dataset comes with some additional information about the labeling process and the text appears in raw form, I have already pre-processed it outside of this notebook.
 
-# In particular, I only maintained the labels and the text, as these are the two variables of interest for this particular report. Aditionally, I already passed the raw text through the transformer blocks of the [all-MiniLM-L6-v2 model](https://huggingface.co/sentence-transformers/all-MiniLM-L6-v2) which was designed to return the sentence embeddings. While it would be interesting to fine-tune this model for the task at hand, I decided against it as I am working on a small cuda-less notebook. Therefore, I only used the model to get the embeddings for each datapoint and we will do the SSL using a custom head for the model.
+# Particularly, I only maintained the labels and the text, as these are the two variables of interest for this report and I already passed the raw text through the transformer blocks of the [all-MiniLM-L6-v2 model](https://huggingface.co/sentence-transformers/all-MiniLM-L6-v2) which was designed to return the sentence embeddings. While it would be interesting to fine-tune this model for the task at hand, I decided against it as I am working on a small cuda-less notebook. Therefore, I only used the model to get the embeddings for each datapoint and we will do the SSL experiments using a custom head for the model.
 
 # For more information on the data preprocessing, see the `process_data.py` script.
 
@@ -22,9 +22,9 @@ import pandas as pd
 
 data = pd.read_csv("data/processed_data.csv")
 # %% [md]
-# While we will of course do the traditional train/dev/test split, in this particular example this comes with a caveat. The three classes are not equally represented and it would be really easy to end up having a test or a dev set nearly (or even completely) lacking a certain label.
+# While we will of course do the traditional train/dev/test split, in this particular case this comes with a caveat. The three classes are not equally represented and it would be really easy to end up having a test or a dev set nearly (or even completely) lacking a certain label.
 
-# A similar problem comes to the surface with the missing labels, as right now, the dataset is not missing any. It is not hard to mask them artifitially in order to run the experiments, however, at say ~90% masking rate it would be really easy to deepen the already deep representation problems.
+# A similar problem comes to the surface with the missing labels, as right now, the dataset is not missing any. It is not hard to mask them artifitially in order to run the experiments, however, at say 99% masking rate it would be really easy to deepen the already deep representation problems.
 
 # %%
 
@@ -36,11 +36,11 @@ data.iloc[:, len(data.columns) - 1].value_counts(normalize=True)
 # 2 - Neither
 
 # %% [md]
-# In order to prevent these issues, I have created 2 custom functions to extract and mask certain proportions of the data label-wise. That is, if I decide to do 50% masking, it will mask 50% of each label, rather then doing it blindly. The same applies to creating splits. Therefore, there will be no need to worry about label representation in any split.
+# In order to prevent these issues, I have created 2 custom functions to extract and mask certain proportions of the data label-wise. That is, if I decide to do 50% masking, it will mask 50% of each label, rather then doing it blindly. The second function works in a similar fashion, however, is used to create splits. Therefore if I want to create a 5% split, it will pull 5% of the datapoints from each category. 
 
-# Having said that, we will create the test split on 10% of the full dataset before masking. The train and dev sizes will be defined dynamically, as we will try the algorythm for different proportions of unlabeled data, however, the dev split will always be 10% of the train split size.
+# Having said that, we will create the test split on 10% of the full dataset before masking. The train and dev sizes will be defined dynamically, as we will try the algorithm for different proportions of the unlabeled data, however, the dev split will always be 10% of the train split size.
 
-# For more information about the functions, see the `experiment_helpers.py`.
+# For more information about these functions, see the `experiment_helpers.py`.
 
 # %%
 from experiment_helpers import mask_labels, extract_equal_proportion
@@ -142,13 +142,20 @@ print(f"Recall on neither: {recall_2:.3f}")
 
 # %% [md]
 
-# Aaaand, I acomplished absolutely nothing with this. I assume I could probably fix this if I torture the loss function enough, adjust the decision treshold or do something to the embeddings but... This was meant to be a 15 minute homework where I would use a library to do everything for me and I have spent... way too much time... on this already. ~40% recall on a 5% minority class is fine, so I am just going to continue. Also, I will keep oversampling for the SSL, because I would like to avoid the minoriry class being an even bigger problem when I start masking. 
+# # Do a test
+
+# %% [md]
+
+# Having established that, we can proceed to the actual SSL experiments.
+
+# We will mask different percentages of the data and then train 2 models in each scenario. One only using the labeled data, and one leveraging the power of self-training algorithm. We will then analyse how the performance of the two compares across the different scenarios. 
+
 
 # %%
 from experiment_helpers import promotion_mechanism
 
 # I will be calling each proportion of masked data a scenario
-scenarios = [0.9, 0.75, 0.5, 0.25, 0.10]
+scenarios = [0.99, 0.9, 0.75, 0.5, 0.25, 0.10, 0.01]
 
 results = pd.DataFrame(
     columns=[
@@ -164,10 +171,9 @@ results = pd.DataFrame(
     ]
 )
 
-row_data = []
-
 for scenario in scenarios:
 
+    row_data = []
     # Define the scenario data
     row_data.append(scenario)
 
@@ -220,10 +226,7 @@ for scenario in scenarios:
 
     while True:
 
-        print("Starting a new SSL iteration")
-
         if len(unlabeled) == 0: # If there is nothing left to promote
-            print("Break called from nothing left to promote")
             break
 
         train_X = train.iloc[:, :-1]
@@ -259,15 +262,8 @@ for scenario in scenarios:
         labeled_after = len(train)
 
         if labeled_before == labeled_after: # If no point promoted, then:
-            print ("Break called from no updates")
             break
 
-        print("promoted " + str(labeled_after - labeled_before) + " datapoints")
-        print(str(len(unlabeled)) + " unlabeled points remain")
-
-
-
-    print("Starting evaluation")
     accuracy, recall_0, recall_1, recall_2 = evaluate_model(
         model=model, predictions=predictions, data=test_X, ground_truth=test_Y
         )
@@ -284,21 +280,73 @@ for scenario in scenarios:
     results.loc[len(results)] = row_data
 
 
-# %%
+# %% [md]
 
-results
-
-# %%
-
-
-experiment_data = data.copy()
-
-experiment_data = mask_labels(experiment_data, mask_probability=0.9)
-
-unlabeled, labeled = extract_equal_proportion(experiment_data, proportion = 1)
-
-print(len(labeled))
-print(len(unlabeled))
-
+# Once the results are collected, we can plot them along each other. Note that as even a single run of the above experiment takes a while on a CPU, I will not be doing hypothesis testing this time. 
 
 # %%
+import matplotlib.pyplot as plt
+
+plt.style.use("seaborn-v0_8-whitegrid")
+
+fig, ax = plt.subplots(nrows = 2, ncols = 2)
+
+# Accuracy
+ax[0,0].set_xlim(0.01, 0.99)
+ax[0,0].set_ylim(0.75, 0.9)
+ax[0,0].set_xlabel("Masked percentage")
+ax[0,0].set_ylabel("Accuracy")
+
+ax[0,0].plot(results["scenario"], results["accNoSSL"], label="Without SSL")
+ax[0,0].plot(results["scenario"], results["accSSL"], label="With SSL")
+ax[0,0].set_xticks([0.01, 0.1, 0.25, 0.5, 0.75, 0.9, 0.99])
+ax[0,0].tick_params(axis="both", labelsize=7)
+
+# Recall 0
+ax[0,1].set_xlim(0.01, 0.99)
+ax[0,1].set_ylim(0.1, 0.6)
+ax[0,1].set_xlabel("Masked percentage")
+ax[0,1].set_ylabel("Recall on Hate speech")
+
+ax[0,1].plot(results["scenario"], results["rec0NoSSL"], label="Without SSL")
+ax[0,1].plot(results["scenario"], results["rec0SSL"], label="With SSL")
+ax[0,1].set_xticks([0.01, 0.1, 0.25, 0.5, 0.75, 0.9, 0.99])
+ax[0,1].tick_params(axis="both", labelsize=7)
+
+# Recall 1
+ax[1,0].set_xlim(0.01, 0.99)
+ax[1,0].set_ylim(0.8, 1)
+ax[1,0].set_xlabel("Masked percentage")
+ax[1,0].set_ylabel("Recall on Offensive language")
+
+ax[1,0].plot(results["scenario"], results["rec1NoSSL"], label="Without SSL")
+ax[1,0].plot(results["scenario"], results["rec1SSL"], label="With SSL")
+ax[1,0].set_xticks([0.01, 0.1, 0.25, 0.5, 0.75, 0.9, 0.99])
+ax[1,0].tick_params(axis="both", labelsize=7)
+
+# Recall 2
+ax[1,1].set_xlim(0.01, 0.99)
+ax[1,1].set_ylim(0.5, 1)
+ax[1,1].set_xlabel("Masked percentage")
+ax[1,1].set_ylabel("Recall on Neither")
+
+ax[1,1].plot(results["scenario"], results["rec2NoSSL"], label="Without SSL")
+ax[1,1].plot(results["scenario"], results["rec2SSL"], label="With SSL")
+ax[1,1].set_xticks([0.01, 0.1, 0.25, 0.5, 0.75, 0.9, 0.99])
+ax[1,1].tick_params(axis="both", labelsize=7)
+
+handles, labels = ax[0,0].get_legend_handles_labels()
+fig.legend(handles, labels, loc="lower center", ncol=2, bbox_to_anchor=(0.5, -0.05))
+
+fig.suptitle("Comparison between using only on the labeles subset vs. using SSL")
+
+plt.style.use("seaborn-v0_8-whitegrid")
+
+plt.tight_layout()
+plt.show()
+
+
+# %% [md]
+
+# We can notice that on this particular dataset, the self-training algorithm doesn't seem to be very helpful. 
+
